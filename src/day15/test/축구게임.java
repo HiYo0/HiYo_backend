13738377c84f6b9440e6d386342b0f0c1e7f9fc5
EunKyeongJang <jjjang1030@naver.com>
2024-01-23 17:22:07 +0900

    day16 end

---
+++ src/day15/test/축구게임.java
@@ -0,0 +1,19 @@
+package day15.test;
+
+public class 축구게임 {
+    public void A버튼() {
+        System.out.println("슛");
+    }
+
+    public void B버튼() {
+        System.out.println("패스");
+    }
+
+    public void C버튼() {
+        System.out.println("달리기");
+    }
+
+    public void D버튼() {
+        System.out.println("ㅇㅇㅇㅇ");
+    }
+}
---
+++ src/day15/test/키우기게임.java
@@ -0,0 +1,18 @@
+package day15.test;
+
+public class 키우기게임 {
+    public void A버튼() {
+        System.out.println("공격");
+    }
+    public void B버튼() {
+        System.out.println("방어");
+    }
+
+    public void C버튼() {
+        System.out.println("체력");
+    }
+
+    public void D버튼() {
+        System.out.println("마법");
+    }
+}
